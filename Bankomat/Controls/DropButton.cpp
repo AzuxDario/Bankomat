8abe077cc1c6a99024491a269f7328aed7c44552
AzuxDario <pawel_mibor95@tlen.pl>
2017-11-29 20:06:08 +0100

    Refactoring

--- Bankomat/Controls/DropButton.cpp
+++ Bankomat/Controls/DropButton.cpp
@@ -6,21 +6,6 @@ DropButton::DropButton(const QString &text, QWidget *parent) : QPushButton(text,
     directory = "";
 }
 
-//--------Getter---------//
-QString DropButton::getDirectory()
-{
-    return directory;
-}
-
-void DropButton::dragEnterEvent(QDragEnterEvent *event)
-{
-    event->acceptProposedAction();
-}
-
-void DropButton::dragMoveEvent(QDragMoveEvent *event)
-{
-    event->acceptProposedAction();
-}
 void DropButton::dropEvent(QDropEvent *event)
 {
     const QMimeData *mimeData = event->mimeData();
