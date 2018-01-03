4027dc8f95d59b512ae0e9bb2726bcb38dff9f37
AzuxDario <pawel_mibor95@tlen.pl>
2018-01-03 22:20:23 +0100

    Add find function

--- Bankomat/Layout/ScreenHolder.cpp
+++ Bankomat/Layout/ScreenHolder.cpp
@@ -4,3 +4,12 @@ ScreenHolder::ScreenHolder()
 {
 
 }
+
+Screen* ScreenHolder::getScreen(ATMState state)
+{
+    if(map->find(state) != map->end())
+    {
+        return map->value(state);
+    }
+    return nullptr;
+}
